688b08518f568fd56c6480d9268cccddb7b63f1c
Syed-Rafi-Naqvi <sd.rafi1819@gmail.com>
2020-05-13 17:57:49 +0530

    added the board with candyColors

--- Candy_Crush/src.js
+++ Candy_Crush/src.js
@@ -1,16 +1,27 @@
-const grid = document.getElementsByClassName('.app')
-const width = 10
-const squares = []
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.app')
+    const width = 10
+    const squares = []
 
 
-function createBoard() {
-    for (let i = 0; i < width * width; i++) {
-        const square = document.createElement('div')
-        grid.appendChild(square)
-        squares.push(square)
+    const candyColors = ['red', 'green', 'blue', 'yellow']
+
+
+    function createBoard() {
+        for (let i = 0; i < width * width; i++) {
+            const square = document.createElement('div')
+            let randomColor = Math.floor(Math.random() * candyColors.length)
+            console.log(randomColor)
+            console.log(candyColors[randomColor])
+            square.style.backgroundColor = candyColors[randomColor]
+            square.setAttribute('draggable', true)
+            square.setAttribute('id', i)
+            grid.appendChild(square)
+            squares.push(square)
+        }
     }
-}
 
-setTimeout(() => {
     createBoard()
-}, 5000);
+})
+
+
